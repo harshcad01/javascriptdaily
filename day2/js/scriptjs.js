bf1a3c6e47c6242a4f95dc61be3d66e0c7f744bf
harshcad01 <harshcad01@gmail.com>
2024-01-05 17:51:40 +0530

    day 2 added

---
+++ day2/js/scriptjs.js
@@ -0,0 +1,26 @@
+console.log("Day 2 javaScript");
+console.log("operators");
+
+
+var a = 20;
+console.log("a value .."+a);
+var b = 40;
+console.log("b value .."+b);
+
+var d= a+b;
+console.log("add .."+d);
+d= a-b;
+console.log("sub .."+d);
+d= a*b;
+console.log("mul .."+d);
+d= a/b;
+console.log("div"+d);
+d= a**b;
+console.log("exp .."+d);
+d= a%b;
+console.log("mod .."+d);
+
+
+
+
+
