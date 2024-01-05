42d8b952b565d0efb4f43b497013624184b006f4
harshcad01 <harshcad01@gmail.com>
2024-01-05 14:36:07 +0530

    files updated

---
+++ day1/js/scriptjs.js
@@ -0,0 +1,30 @@
+console.log("javascript day 1");
+console.log("declaring a varable");
+var a= 10;
+let b= 20;
+const c =30;
+console.log(a);
+console.log(b);
+var d = a+b+c;
+console.log("added value is : "+d);
+
+
+a=40;
+b=50;
+d=a-b-c;
+console.log(a);
+console.log(b);
+console.log("subtracted value is : "+d)
+
+
+
+a={fname:'harsh', Age:20};
+console.log(a);
+console.table(a);
+
+
+
+
+b = ["harsh","aravind","aarif","abinaya","anand","dinesh","natrayan"];
+console.log(b);
+console.table(b)
