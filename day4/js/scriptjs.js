64ba7755d03229a87a9aa29bda25d25c1c9e4555
harshcad01 <harshcad01@gmail.com>
2024-01-09 17:58:45 +0530

    updated

--- day4/js/scriptjs.js
+++ day4/js/scriptjs.js
@@ -41,8 +41,44 @@ function grades(m){
     }    
     else if(35<=m && m<=100){
         console.log("pass");
+    }else if(m>100){
+        console.log("Your exeding the limites")
     }else{
         console.log("Enter the mark");
     }
 }
-grades()
+grades(100);
+
+
+
+function grades1(m){
+    var a= Number(prompt("Enter the first num"));
+    var b= Number(prompt("Enter the second num"));
+    switch(m){
+        case "+":
+            var c =a+b;
+            console.log("added value is .."+c);
+            break;
+        case "-":
+            var c= a-b;
+            console.log("sutracted value is .."+c);
+            break;
+        case "*":
+            var c = a*b;
+            console.log("Multiplied value is .."+c);
+            break;
+            case "/":
+                var c = a/b;
+                console.log("divided value is .."+c);
+                break;
+            case "%":
+                var c = a%b;
+                console.log("Reminder value is .."+c);
+                break;
+        default:
+            alert("case not in list");
+    }
+
+}
+
+grades1(prompt("operation add , sub, mul"));
