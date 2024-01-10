454eb52801d6fc22624e0a6238f4106bc74dbbe0
harshcad01 <harshcad01@gmail.com>
2024-01-10 17:39:03 +0530

    day6 added

--- day4/js/scriptjs.js
+++ day4/js/scriptjs.js
@@ -51,34 +51,3 @@ grades(100);
 
 
 
-function grades1(m){
-    var a= Number(prompt("Enter the first num"));
-    var b= Number(prompt("Enter the second num"));
-    switch(m){
-        case "+":
-            var c =a+b;
-            console.log("added value is .."+c);
-            break;
-        case "-":
-            var c= a-b;
-            console.log("sutracted value is .."+c);
-            break;
-        case "*":
-            var c = a*b;
-            console.log("Multiplied value is .."+c);
-            break;
-            case "/":
-                var c = a/b;
-                console.log("divided value is .."+c);
-                break;
-            case "%":
-                var c = a%b;
-                console.log("Reminder value is .."+c);
-                break;
-        default:
-            alert("case not in list");
-    }
-
-}
-
-grades1(prompt("operation add , sub, mul"));
---
+++ day6/js/scriptjs.js
@@ -0,0 +1,30 @@
+function calculate(){
+    var a= parseFloat(document.getElementById("num1").value);
+    var b= parseFloat(document.getElementById("num2").value);
+    var R;
+    var opt = ['+','-','*']
+    switch(opt){
+        case "+":
+            R =a+b;
+            break;
+        case "-":
+            R= a-b;
+            break;
+        case "*":
+            R= a*b;
+            break;
+            case "/":
+            R= a/b;
+                break;
+            case "%":
+            R= a%b;
+                break;
+        default:
+            alert("Invalid Operation");
+    }
+
+    document.getElementById("result").innerHTML = R ;
+
+}
+
+grades1(R);
