67d3a15798086ecb343e61bda3f1650fb8e2bccc
harshcad01 <harshcad01@gmail.com>
2024-01-11 17:19:41 +0530

    changes made

--- day7/js/scriptjs.js
+++ day7/js/scriptjs.js
@@ -1,10 +1,3 @@
-function calculate(event){
-        event.preventDefault();
-    var a= parseFloat(document.getElementById("num1").value);
-    var b= parseFloat(document.getElementById("num2").value);
-}
-
-
 function calculation(opt){
     var a= parseFloat(document.getElementById("num1").value);
     var b= parseFloat(document.getElementById("num2").value);
@@ -27,6 +20,7 @@ function calculation(opt){
         default:
             alert("Invalid Operation");
     }
+    document.getElementById("result").innerHTML = R ;
 }
 
-
+grades1(R);
