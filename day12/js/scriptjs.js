a209a50dac04a7de13882ce31e8718b275e2d3c4
harshcad01 <harshcad01@gmail.com>
2024-01-31 18:08:17 +0530

    changes made

--- day12/js/scriptjs.js
+++ day12/js/scriptjs.js
@@ -11,11 +11,11 @@ console.log(cars.model);
 
 console.log("Creating a function in side the object");
 //Adding a method to the object
-function Mypizza() {
+
   const pizza = {
-    type: document.getElementById("pizzatype").value,
-    size: document.getElementById("pizzasiza").value,
-    toppings: document.getElementById("pizzatop").value,
+    type: "margherita",
+    size: "small",
+    toppings: "double cheese",
     bake: function () {
       const message = `baking a ${this.type} ${this.size} crust ${this.toppings} pizza.`;
       console.log(message);
@@ -23,8 +23,9 @@ function Mypizza() {
     }
   };
 
-  document.getElementById("value").innerHTML = pizza.bake();
-}
+ pizza.bake();
+
+
 
 
 
